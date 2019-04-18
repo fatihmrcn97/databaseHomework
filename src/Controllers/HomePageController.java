863ab3c913b9517397b6e0a7773d1b2832d31f6e
Fatih Mercan <fatihmrcn97@gmail.com>
2019-04-18 15:07:57 +0300

    LogOut and Exit button done

--- src/Controllers/HomePageController.java
+++ src/Controllers/HomePageController.java
@@ -5,6 +5,8 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,9 +15,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.Node;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 
 public class HomePageController implements Initializable {
 
@@ -35,6 +38,11 @@ public class HomePageController implements Initializable {
     @FXML
     private Button contact;
 
+    @FXML
+    private Button ex1;
+
+    @FXML
+    private Button logO1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -60,6 +68,26 @@ public class HomePageController implements Initializable {
         }
     }
 
+    @FXML
+    void ex(ActionEvent event) {
+        Platform.exit();
+    }
 
+    @FXML
+    void logOut(ActionEvent event) {
+        try {
+
+            logO1.getScene().getWindow().hide();
+            Stage login = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("LoginMain.fxml"));
+            Scene scene = new Scene(root);
+            login.setScene(scene);
+            login.show();
+            login.setResizable(false);
+            scene.getStylesheets().add("/Controllers/style.css");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 }
